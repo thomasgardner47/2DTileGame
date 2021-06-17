966b0d6a4a97dc2ee2fa318528d7a3c3378e6021
thomasgardner47 <64431748+thomasgardner47@users.noreply.github.com>
2021-06-17 08:22:55 +0100

    Created main framework of game
    added game loop

--- TileGame/src/dev/codenmore/tilegame/Game.java
+++ TileGame/src/dev/codenmore/tilegame/Game.java
@@ -2,21 +2,76 @@ package dev.codenmore.tilegame;
 
 
 // the main class for the game containing the most important code
-public class Game {
+public class Game implements Runnable {
 
     // game class will need an instance of Display class
     private Display display;
-
     public int width, height;
+    public String title;
+
+    private boolean running = false;
+    private Thread thread;
+
+    private void init() {
+        display = new Display(title, width, height);
+    }
+
+    private void tick() {
+
+    }
+
+    private  void render() {
+
+    }
 
     // creation of game constructor
     public Game(String title, int width, int height) {
 
         this.width = width;
         this.height = height;
+        this.title = title;
 
         // intialise instance of Display class inside Game constructor
-        display = new Display(title, width, height);
+
+    }
+
+    public void run() { // in order to allow this class too implement Runnable which in turn enables threading we need a public run method
+
+        init();
+
+        while (running) { // while running is true we want the tick and render methods to keep running
+            tick();
+            render();
+        }
+
+        Stop(); // just incase the game doesn't stop the first time
+
+
+    }
+
+    // only use the synchronized keyword when working will threads directly
+    public synchronized void start() { // will start up thread
+
+        if (running) {
+            return;
+        }
+        thread = new Thread(this);
+        thread.start();
+    }
+
+    public synchronized void Stop() { // will stop thread
+        if (!running){
+            return;
+        }
+
+        running = false;
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
     }
 
 }
